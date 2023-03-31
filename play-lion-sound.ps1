0384e70a4732ff46a1c021498dd39cd38267b55d
Michael Surtees <msurtees@me.com>
2023-03-31 08:26:22 -0400

    Add files via upload

---
+++ play-lion-sound.ps1
@@ -0,0 +1,16 @@
+﻿<#
+.SYNOPSIS
+	Plays a lion sound
+.DESCRIPTION
+	This PowerShell script plays a lion sound.
+.EXAMPLE
+	PS> ./play-lion-sound
+	(listen and enjoy)
+.NOTES
+	Author: Markus Fleschutz / License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/play-mp3.ps1" "$PSScriptRoot/../Data/Sounds/lion.mp3"
+exit 0 # success
---
+++ play-parrot-sound.ps1
@@ -0,0 +1,16 @@
+﻿<#
+.SYNOPSIS
+	Plays a parrot sound
+.DESCRIPTION
+	This PowerShell script plays a parrot sound.
+.EXAMPLE
+	PS> ./play-parrot-sound
+	(listen and enjoy)
+.NOTES
+	Author: Markus Fleschutz / License: CC0
+.LINK
+	https://github.com/fleschutz/PowerShell
+#>
+
+& "$PSScriptRoot/play-mp3.ps1" "$PSScriptRoot/../Data/Sounds/parrot.mp3"
+exit 0 # success
