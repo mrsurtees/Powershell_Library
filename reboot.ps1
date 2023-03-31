da142c1992dc7ca104c31f5482f01912d6620a8f
Michael Surtees <msurtees@me.com>
2023-03-31 08:20:24 -0400

    update reboot

---
+++ reboot.ps1
@@ -0,0 +1,22 @@
+﻿<#
+.SYNOPSIS
+	Reboots the local computer (needs admin rights)
+.DESCRIPTION
+	This PowerShell script reboots the local computer (needs admin rights).
+.EXAMPLE
+	PS> ./reboot
+.LINK
+	https://github.com/fleschutz/PowerShell
+.NOTES
+	Author: Markus Fleschutz / License: CC0
+#>
+
+#Requires -RunAsAdministrator
+
+try {
+	Restart-Cobmputer
+	exit 0 # success
+} catch {
+	"⚠️ Error in line $($_.InvocationInfo.ScriptLineNumber): $($Error[0])"
+	exit 1
+}
