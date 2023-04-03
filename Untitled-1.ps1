5bb874365c6afa71b0af179445adc981a73d7269
Michael Surtees <msurtees@me.com>
2023-04-03 10:34:31 -0400

    bs update

---
+++ Untitled-1.ps1
@@ -0,0 +1,9 @@
+Get-ChildItem -Path "C:\Users\msurtees\Desktop\2022 Tableau\" -File | ForEach-Object { Rename-Item $_.FullName -NewName ($_.name).Replace(".ps1", ".txt") }
+Copy-Item -Path "C:\Users\msurtees\Desktop\2022 Tableau\*.txt" -Recurse '\\Mac\Home\Library\Application Support\DEVONthink 3\Inbox'
+Get-ChildItem -Path "C:\Users\msurtees\Desktop\2022 Tableau\" -File | ForEach-Object { Rename-Item $_.FullName -NewName ($_.name).Replace(".txt", ".ps1") }
+
+Copy-Item 'C:\Users\msurtees\Desktop\Copy PS! Files as text.ps1' '\\Mac\Home\Library\Application Support\DEVONthink 3\Inbox'
+$
+ = @{
+    Name = Value
+}
