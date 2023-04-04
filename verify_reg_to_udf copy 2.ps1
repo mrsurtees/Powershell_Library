3357b095085944b779c49e91f3164995dfa249ba
Michael Surtees <msurtees@me.com>
2023-04-04 13:43:00 -0400

    More duplicate file deletions

--- verify_reg_to_udf copy 2.ps1
+++
@@ -1,9 +0,0 @@
-﻿#Set-ItemProperty REGISTRY::HKLM\SOFTWARE\Classes\SOFTWARE\think-cell -Name licensekey -Value NG3XR-3BXMW-S2ZNG-LFM4X-95XF5
-$regcheck = Get-ItemProperty REGISTRY::HKLM\SOFTWARE\Classes\SOFTWARE\think-cell -name licensekey # | Out-File C:\temp\name.txt
-if ($regcheck -like '*NG3*' )
-{
-  $verified = "Custom"+'9' 
-  New-ItemProperty -Path "HKLM:\SOFTWARE\CentraStage" -Name $verified  -PropertyType String -value $regcheck
-}
-
-#New-ItemProperty -Path HKLM:\SOFTWARE\CentraStage -Name $verified -PropertyType String -value "V"
